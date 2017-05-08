358564fb359b5c610ba6635719dd0def77bd72bb
stvkre <kiariemburuh@gmail.com>
2017-05-08 12:13:29 +0300

    Monster.java
    Define class Monster with name

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,5 +1,11 @@
 public class Monster {
+  private String name;
+
   public Monster(String name, int personId) {
-    
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
   }
 }
